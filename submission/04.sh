6c0e19c98432bf3caf385e6f6c58bb9b0177d0f0
Ayoazeez26 <44169294+Ayoazeez26@users.noreply.github.com>
2026-04-15 23:22:00 +0100

    feat: fixed 04

--- submission/04.sh
+++ submission/04.sh
@@ -6,14 +6,17 @@ timestamp=1495584032
 
 timestamp_le=$(printf '%08x' $timestamp | sed 's/\(..\)\(..\)\(..\)\(..\)/\4\3\2\1/')
 
-timestamp_push_len="04"
+pubkey_hash=$(echo "$publicKey" | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -ripemd160 -binary | xxd -p -c 20)
 
-pubkey_push_len="21"
-
+PUSH_4="04"
 OP_CLTV="b1"
 OP_DROP="75"
+OP_DUP="76"
+OP_HASH160="a9"
+PUSH_20="14"
+OP_EQUALVERIFY="88"
 OP_CHECKSIG="ac"
 
-cltv_script="${timestamp_push_len}${timestamp_le}${OP_CLTV}${OP_DROP}${pubkey_push_len}${publicKey}${OP_CHECKSIG}"
+cltv_script="${PUSH_4}${timestamp_le}${OP_CLTV}${OP_DROP}${OP_DUP}${OP_HASH160}${PUSH_20}${pubkey_hash}${OP_EQUALVERIFY}${OP_CHECKSIG}"
 
 bitcoin-cli -regtest decodescript "$cltv_script"
