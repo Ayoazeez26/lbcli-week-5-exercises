d18f9367c97aa26e332a292048d035be49792665
Ayoazeez26 <44169294+Ayoazeez26@users.noreply.github.com>
2026-04-15 23:44:43 +0100

    feat: fixed 05

--- submission/05.sh
+++ submission/05.sh
@@ -4,16 +4,26 @@ publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
 blocks=150
 
-blocks_le=$(printf '%06x' $blocks | sed 's/\(..\)\(..\)\(..\)/\3\2\1/')
+blocks_le=$(printf '%04x' $blocks | sed 's/\(..\)\(..\)/\2\1/')
 
-blocks_push_len="03"
+blocks_push_len="02"
 
+pubkey_hash=$(python3 -c "
+import hashlib
+pk = bytes.fromhex('$publicKey')
+sha = hashlib.sha256(pk).digest()
+print(hashlib.new('ripemd160', sha).digest().hex())
+")
+
+
+OP_DUP="76"
+OP_HASH160="a9"
+PUSH_20="14"
+OP_EQUALVERIFY="88"
 OP_CSV="b2"
 OP_DROP="75"
 OP_CHECKSIG="ac"
 
-pubkey_push_len="21"
-
-csv_script="${blocks_push_len}${blocks_le}${OP_CSV}${OP_DROP}${pubkey_push_len}${publicKey}${OP_CHECKSIG}"
+csv_script="${blocks_push_len}${blocks_le}${OP_CSV}${OP_DROP}${OP_DUP}${OP_HASH160}${PUSH_20}${pubkey_hash}${OP_EQUALVERIFY}${OP_CHECKSIG}"
 
 echo "$csv_script"
